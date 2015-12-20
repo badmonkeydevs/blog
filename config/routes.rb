9b320b2b980d20c5d14649f6ee0c26efc56b6586
Michael Kelly <michaelkelly322@gmail.com>
2015-12-20 03:30:24 -0500

    Adds basic about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#landing'
+  get 'about', to: 'pages#about'
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,3 +6,15 @@ feature 'Landing page' do
     expect(page).to have_content('Blicker')
   end
 end
+
+feature 'About page' do
+  scenario 'Shows site title' do
+    visit about_path
+    expect(page).to have_content('Blicker')
+  end
+
+  scenario 'Shows about text' do
+    visit about_path
+    expect(page).to have_content('a site to collect and monitor stock symbols while I blog about them')
+  end
+end
